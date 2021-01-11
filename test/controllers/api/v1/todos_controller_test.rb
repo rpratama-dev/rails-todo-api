7b9efb8f1cd9c345280be1b5b139c97037878d03
rpratama94 <rpratama.sky@gmail.com>
2021-01-11 10:14:17 +0700

    Create Routes and Controller

---
+++ test/controllers/api/v1/todos_controller_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class Api::V1::TodosControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_v1_todos_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get api_v1_todos_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get api_v1_todos_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get api_v1_todos_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get api_v1_todos_destroy_url
+    assert_response :success
+  end
+end
